d81cef51cd28d5d741c6657a5ee916f952e29d81
F H <fhuang.uk@gmail.com>
2025-07-26 15:54:25 +0100

    connected with frontend

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const pool = require("./src/config/db.js");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 const authRoutes = require("./src/routes/authRoutes.js");
 
