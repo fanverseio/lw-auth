35b06a6ead839c92060a6f39383a070883db2d03
F H <fhuang.uk@gmail.com>
2025-08-24 22:20:53 +0100

    updated domain

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      process.env.FRONTEND_URL || "http://localhost:5173",
+      "https://learnerweave.space",
+      "https://www.learnerweave.space",
+      "lw-frontend.vercel.app",
+    ],
     credentials: true,
   })
 );
